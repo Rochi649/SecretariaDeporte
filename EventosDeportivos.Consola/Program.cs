5081519c3d1b9e990eaba909638a2669613d8502
Leonardo Perdomo <leonardop763@gmail.com>
2021-09-22 01:33:37 -0500

    Se añade la funcionalidad de inscribir equipos a torneos por consola, al mismo tiempo se realizan pruebas en el frontend con cshtml

--- EventosDeportivos.Consola/Program.cs
+++ EventosDeportivos.Consola/Program.cs
@@ -10,6 +10,9 @@ namespace EventosDeportivos.Consola
         private static IRepositorioMunicipio _repositorioMunicipio = new RepositorioMunicipio(new EventosDeportivos.Persistencia.AppContext());
         private static IRepositorioTorneo _repositorioTorneo = new RepositorioTorneo(new EventosDeportivos.Persistencia.AppContext());
         private static IRepositorioEscenario _repositorioEscenario = new RepositorioEscenario(new EventosDeportivos.Persistencia.AppContext());
+        private static IRepositorioPatrocinador _repositorioPatrocinador = new RepositorioPatrocinador(new EventosDeportivos.Persistencia.AppContext());
+        private static IRepositorioEquipo _repositorioEquipo = new RepositorioEquipo(new EventosDeportivos.Persistencia.AppContext());
+        private static IRepositorioTorneoEquipo _repositorioTorneoEquipo = new RepositorioTorneoEquipo(new EventosDeportivos.Persistencia.AppContext());
         static void Main(string[] args)
         {
             /*
@@ -83,9 +86,39 @@ namespace EventosDeportivos.Consola
                 Console.WriteLine("Ha ocurrido un error en la creacion del escenario");
             }
             
-            */
-
+            
             listarEscenarios();
+
+            
+
+            bool creado = crearPatrocinador();
+
+            if (creado)
+            {
+                Console.WriteLine("El patrocinador fue creado");
+            }
+            else
+            {
+                Console.WriteLine("El patrocinador no fue creado");
+            }
+
+            
+
+            crearEquipo();
+            */
+            inscribirEquipo();
+            
+            listarTorneos();
+            Console.WriteLine("----------------------------------------------------------------");
+            listarEquipos();
+            Console.WriteLine("----------------------------------------------------------------");
+            listarMunicipios();
+            Console.WriteLine("----------------------------------------------------------------");
+            listarPatrocinadores();
+            Console.WriteLine("----------------------------------------------------------------");
+            listarEscenarios();
+            Console.WriteLine("----------------------------------------------------------------");
+
         }
 
         private static bool crearMunicipio()
@@ -175,7 +208,7 @@ namespace EventosDeportivos.Consola
             int municipioId = int.Parse(Console.ReadLine());
 
 
-            var torneo = new Torneo
+            bool creado = _repositorioTorneo.CrearTorneo(new Torneo
             {
                 Nombre = nombre,
                 Categoria = categoria,
@@ -183,8 +216,7 @@ namespace EventosDeportivos.Consola
                 FechaFin = fechaFinal,
                 Tipo = tipo,
                 MunicipioId = municipioId
-            };
-            bool creado = _repositorioTorneo.CrearTorneo(torneo);
+            });
 
             return creado;
 
@@ -193,12 +225,12 @@ namespace EventosDeportivos.Consola
         private static void listarTorneos()
         {
             IEnumerable<Torneo> torneos = _repositorioTorneo.ListarTorneos();
-            Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-10} |", "Id", "Nombre", "Categoria", "Tipo"));
-            Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-10} |", "-----", "---------------------", "----------------", "---------"));
+            Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-16} |", "Id", "Nombre", "Categoria", "Tipo"));
+            Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-16} |", "-----", "---------------------", "----------------", "---------"));
             foreach (var torneo in torneos)
             {
-                Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-10} |", torneo.Id, torneo.Nombre, torneo.Categoria, torneo.Tipo));
-                Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-10} |", "-----", "---------------------", "----------------", "---------"));
+                Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-16} |", torneo.Id, torneo.Nombre, torneo.Categoria, torneo.Tipo));
+                Console.WriteLine(String.Format("{0,-5} | {1,-22} | {2,-16} | {3,-16} |", "-----", "---------------------", "----------------", "---------"));
             }
 
 
@@ -241,6 +273,12 @@ namespace EventosDeportivos.Consola
 
         }
 
+        private static Torneo buscarTorneo(int idTorneo)
+        {
+            Torneo torneo = _repositorioTorneo.BuscarTorneo(idTorneo);
+
+            return torneo;
+        }
         private static bool crearEscenario()
         {
             Console.WriteLine("Ingrese el nombre del escenario: ");
@@ -283,5 +321,124 @@ namespace EventosDeportivos.Consola
 
 
         }
+
+        private static bool crearPatrocinador()
+        {
+            Console.WriteLine("Ingrese el numero de identificacion: ");
+            string identificacion = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre del patrocinador: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el tipo de persona del patrocinador: ");
+            string tipodepersona = Console.ReadLine();
+            Console.WriteLine("Ingrese la direccion del patrocinador: ");
+            string direccion = Console.ReadLine();
+
+            var patrocinador = new Patrocinador
+            {
+                Identificacion = identificacion,
+                Nombre = nombre,
+                TipoPersona = tipodepersona,
+                Direccion = direccion
+            };
+
+            bool creado = _repositorioPatrocinador.CrearPatrocinador(patrocinador);
+
+            return creado;
+        }
+
+        private static void listarPatrocinadores()
+        {
+            IEnumerable<Patrocinador> patrocinadores = _repositorioPatrocinador.ListarPatrocinadores();
+            Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-24} | {4,-16}|", "Id", "Identificacion", "Nombre", "Direccion", "Tipo de Persona"));
+            Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-24} | {4,-16}|", "-----", "-----------------", "----------------", "-------------------", "--------"));
+            foreach (var patrocinador in patrocinadores)
+            {
+                Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-24} | {4,-16}|", patrocinador.Id, patrocinador.Identificacion, patrocinador.Nombre, patrocinador.Direccion, patrocinador.TipoPersona));
+                Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-24} | {4,-16}|", "-----", "-----------------", "----------------", "-------------------", "--------"));
+            }
+        }
+
+        private static bool crearEquipo()
+        {
+            Console.WriteLine("Ingrese el nombre del equipo: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la cantidad de deportistas que posee el equipo: ");
+            int cantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el deporte que practica el equipo: ");
+            string deporte = Console.ReadLine();
+            Console.WriteLine("Especifique el patrocinador del equipo: ");
+            listarPatrocinadores();
+            int patrocinadorId = int.Parse(Console.ReadLine());
+
+
+            bool creado = _repositorioEquipo.CrearEquipo(new Equipo
+            {
+                Nombre = nombre,
+                CantidadDeportistas = cantidad,
+                Deporte = deporte,
+                PatrocinadorId = patrocinadorId
+            });
+
+            return creado;
+        }
+
+        private static Equipo buscarEquipo(int idEquipo)
+        {
+            Equipo equipo = _repositorioEquipo.BuscarEquipo(idEquipo);
+
+            return equipo;
+        }
+
+        private static void listarEquipos()
+        {
+            IEnumerable<Equipo> equipos = _repositorioEquipo.ListarEquipos();
+            Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-28} | {4,-20}|", "Id", "Nombre", "Cantidad De Deportistas", "Deporte", "Id del Patrocinador"));
+            Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-28} | {4,-20}|", "-----", "-----------------", "------------------", "-------------------", "--------"));
+            foreach (var equipo in equipos)
+            {
+                Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-28} | {4,-20}|", equipo.Id, equipo.Nombre, equipo.CantidadDeportistas, equipo.Deporte, equipo.PatrocinadorId));
+                Console.WriteLine(String.Format("{0,-5} | {1,-18} | {2,-20} | {3,-28} | {4,-20}|", "-----", "-----------------", "------------------", "-------------------", "--------"));
+            }
+        }
+
+        private static void inscribirEquipo()
+        {
+            Console.WriteLine("Elija el equipo: ");
+            listarEquipos();
+            int idEquipo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elija el torneo al cual desea inscribir al equipo: ");
+            listarTorneos();
+            int idTorneo = int.Parse(Console.ReadLine());
+
+            bool inscrito = _repositorioTorneoEquipo.InscribirEquipoATorneo(idEquipo, idTorneo);
+
+            if (inscrito)
+            {
+                Equipo equipo = buscarEquipo(idEquipo);
+                Torneo torneo = buscarTorneo(idTorneo);
+
+                Console.WriteLine("{0} ha sido inscrito al torneo {1}", equipo.Nombre, torneo.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("Ha ocurrido un error durante la inscripcion");
+            }
+
+        }
+
+        /*
+        private static void listarInscritos()
+        {
+            IEnumerable<TorneoEquipo> inscritos = _repositorioTorneoEquipo.ListarInscritos();
+            Console.WriteLine(String.Format(" {0,-18} | {1,-20} | ", "Equipo", "Torneo"));
+            Console.WriteLine(String.Format(" {0,-18} | {1,-20} | ", "-----------------", "------------------"));
+            foreach (var inscrito in inscritos)
+            {
+
+                Console.WriteLine(String.Format(" {0,-18} | {1,-20} | ", );
+                Console.WriteLine(String.Format(" {0,-18} | {1,-20} | ", "-----------------", "------------------"));
+            }
+        }
+        */
     }
 }
